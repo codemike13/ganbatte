d5deb315cbdd017f85466042e9af14b16fd5cf29
Anthony Michael Stephens <mike.stephens@codeimmersives.com>
2019-12-13 14:32:20 -0500

    Added front and back end ejs and react for ganbatte

---
+++ ganbatte-front/src/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+class App extends Component {
+
+    state = {
+
+    }
+
+    render() {
+        return (
+            <>
+                <div className={'container'}></div>
+            </>
+        )
+
+    }
+
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
