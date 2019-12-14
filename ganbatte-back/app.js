8f0709f0dc6342f9276797825470777aef5eeaae
Anthony Michael Stephens <mike.stephens@codeimmersives.com>
2019-12-14 15:14:39 -0500

    Updates to routes

--- ganbatte-back/app.js
+++ ganbatte-back/app.js
@@ -1,18 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose')
+const passport = require('passport')
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users/users');
 
-var app = express();
+require('dotenv').config()
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(`Error: ${err}`))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(passport.initialize())
+
+require('./passport/passport')(passport)
+
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,14 +38,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/todo', todoRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- ganbatte-back/routes/users/models/User.js
+++ ganbatte-back/routes/users/models/User.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
     email: { type: String, default: '' },
     password: { type: String, default: '' },
-    todos: [{ type: Schema.Types.ObjectId, ref: 'todo' }]
 })
 
 module.exports = mongoose.model('user', UserSchema)
